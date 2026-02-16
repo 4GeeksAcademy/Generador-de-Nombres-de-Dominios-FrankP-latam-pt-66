ca1a35591fcbbade1c4b369170885711280a5ca4
FrankGre <73871005+FrankGre@users.noreply.github.com>
2026-02-16 13:55:44 +0000

    Generador de dominios completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import "bootstrap";
-import "./style.css";
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io']; // El reto extra ;)
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            for (let e = 0; e < extensions.length; e++) {
+                
+                console.log(`${pronoun[p]}${adj[a]}${noun[n]}${extensions[e]}`);
+            }
+        }
+    }
